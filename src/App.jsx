8d56b8bf928d674ea26c1231b1d1887740af011f
GabrielRouillet <rouilletgabriel@gmail.com>
2024-09-24 19:51:42 -0300

    views

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeView from './views/HomeView/HomeView'
+import ProducsView from './views/ProducsView/ProducsView'
+import ContactView from './views/ContactView/ContactView'
 
 function App() {
 
   return (
     <>
-<NavBar></NavBar>
-<ItemListContainer></ItemListContainer>
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <Routes>
+          <Route exact path='/' element={<HomeView/>} />
+          <Route exact path='/' element={<ProducsView/>} />
+          <Route exact path='/' element={<ContactView/>} />
+        </Routes>
+        <ItemListContainer></ItemListContainer>
+      </BrowserRouter>
     </>
   );
 }
