37209ef55b4583ffe9c579367aa0cf89f7907008
GabrielRouillet <rouilletgabriel@gmail.com>
2024-09-24 20:05:04 -0300

    cambios en views

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ItemListContainer from "./components/ItemListContainer"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeView from './views/HomeView/HomeView'
 import ProducsView from './views/ProducsView/ProducsView'
-import ContactView from './views/ContactView/ContactView'
+import ContactView from './views/ContactView/ContactView';
 
 function App() {
 
