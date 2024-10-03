4b5cf5a0eb65e48db4f9ceca430ef0f6dc87f87e
GabrielRouillet <rouilletgabriel@gmail.com>
2024-10-03 20:00:58 -0300

    contador

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -2,37 +2,40 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchProducts } from './mockApi';
 import './ItemDetailContainer.css';
+import ContadorComponente from './ContadorComponente';
+
 
 const ItemDetailContainer = () => {
-  const { id } = useParams();
-  const [product, setProduct] = useState(null);
-  const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const loadProduct = async () => {
-      const products = await fetchProducts();
-      const foundProduct = products.find((product) => product.id === parseInt(id));
-      setProduct(foundProduct);
-      setLoading(false);
-    };
+    useEffect(() => {
+        const loadProduct = async () => {
+            const products = await fetchProducts();
+            const foundProduct = products.find((product) => product.id === parseInt(id));
+            setProduct(foundProduct);
+            setLoading(false);
+        };
 
-    loadProduct();
-  }, [id]);
+        loadProduct();
+    }, [id]);
 
-  if (loading) {
-    return <div>Cargando detalles del producto...</div>;
-  }
+    if (loading) {
+        return <div>Cargando detalles del producto...</div>;
+    }
 
-  return (
-    <div className="detail-container">
-    <h1 className="product-name">{product.nombre}</h1>
-    <div className="image-container">
-      <img src={product.img} alt={product.nombre} className="product-image" />
-    </div>
-    <p className="product-price">Precio: ${product.precio}</p>
-    <p className="product-description">{product.descripcion}</p>
-  </div>
-);
+    return (
+        <div className="detail-container">
+            <h1 className="product-name">{product.nombre}</h1>
+            <div className="image-container">
+                <img src={product.img} alt={product.nombre} className="product-image" />
+            </div>
+            <p className="product-price">Precio: ${product.precio}</p>
+            <p className="product-description">{product.descripcion}</p>
+            <div><ContadorComponente></ContadorComponente></div>
+        </div>
+    );
 };
 
 export default ItemDetailContainer;
